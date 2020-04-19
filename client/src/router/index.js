be544669a8ce524de684780e39eac9994718cfdb
eatthatpie <eatthatpie.dev@gmail.com>
2020-04-19 08:01:55 +0200

    small tweaks

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -19,7 +19,13 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior() {
+    return {
+      x: 0,
+      y: 0
+    }
+  }
 })
 
 export default router
